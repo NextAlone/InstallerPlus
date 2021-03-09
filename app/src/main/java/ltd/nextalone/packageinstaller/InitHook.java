c2ce7e7a27951cdc45424acdd59bded8e46b367f
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-03-09 14:42:28 +0800

    Update

---
+++ app/src/main/java/ltd/nextalone/packageinstaller/InitHook.java
@@ -0,0 +1,17 @@
+package ltd.nextalone.packageinstaller;
+
+import android.util.Log;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+class InitHook implements IXposedHookLoadPackage {
+    private static final String TAG = "NextAlone";
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        Log.d(TAG, "handleLoadPackage:" + lpparam.packageName);
+        if (!lpparam.packageName.equals("ltd.nextalone.packageinstaller")) return;
+        Log.d(TAG, "Hooked");
+    }
+}
--- app/src/main/java/ltd/nextalone/packageinstaller/MainHook.kt
+++ app/src/main/java/ltd/nextalone/packageinstaller/MainHook.kt
@@ -1,27 +1,20 @@
-package ltd.nextalone.packageInstaller
+package ltd.nextalone.packageinstaller
 
 import android.util.Log
-import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-class MainHook : IXposedHookLoadPackage {
+class MainHook  {
     private val name = "carlos"
 
     fun printlnName() {
         Log.d(TAG, "printlnName: $name")
     }
 
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        Log.d(TAG, "handleLoadPackage: ${lpparam.packageName}")
-        if (lpparam.packageName != "ltd.nextalone.packageInstaller") return
-        hookPackageInstaller(lpparam)
-    }
-
     private fun hookPackageInstaller(lpparam: XC_LoadPackage.LoadPackageParam) {
         Log.d(TAG, "hookPackageInstaller: ")
         val hookClass =
-            lpparam.classLoader.loadClass("ltd.nextalone.packageInstaller.MainHook") ?: return
+            lpparam.classLoader.loadClass("ltd.nextalone.packageinstaller.MainHook") ?: return
         // 通过XposedHelpers调用静态方法printlnHelloWorld
         XposedHelpers.callStaticMethod(hookClass, "printlnHelloWorld")
         val demoClass = hookClass.newInstance()
