86494cab54db9a6fb777be32f76954672b3badc4
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-03-10 01:41:27 +0800

    Initial support for Android 11

--- app/src/main/java/ltd/nextalone/pkginstallerplus/HookEntry.java
+++ app/src/main/java/ltd/nextalone/pkginstallerplus/HookEntry.java
@@ -1,30 +1,39 @@
 package ltd.nextalone.pkginstallerplus;
 
+import android.os.Build.VERSION;
 import android.util.Log;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import ltd.nextalone.pkginstallerplus.sdk25.PackageInstallerActivityHook;
+import ltd.nextalone.pkginstallerplus.sdk30.PackageInstallerActivityHook30;
 
-public class InitHook implements IXposedHookLoadPackage {
+public class HookEntry implements IXposedHookLoadPackage {
+
     private static final String TAG = "NextAlone";
 
     private static boolean sInitialized = false;
+
+    private static void initializeHookInternal(XC_LoadPackage.LoadPackageParam lpparam) {
+        Log.d(TAG, "Hooked");
+        switch (VERSION.SDK_INT) {
+            case 25:
+                PackageInstallerActivityHook.INSTANCE.initOnce(lpparam.classLoader);
+                break;
+            case 30:
+                PackageInstallerActivityHook30.INSTANCE.initOnce(lpparam.classLoader);
+                break;
+        }
+    }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         Log.d(TAG, "handleLoadPackage: " + lpparam.packageName);
         if ("com.google.android.packageinstaller".equals(lpparam.packageName)
-                || "com.android.packageinstaller".equals(lpparam.packageName)) {
+            || "com.android.packageinstaller".equals(lpparam.packageName)) {
             if (!sInitialized) {
                 sInitialized = true;
                 initializeHookInternal(lpparam);
             }
         }
-    }
-
-    private static void initializeHookInternal(XC_LoadPackage.LoadPackageParam lpparam) {
-        Log.d(TAG, "Hooked");
-        PackageInstallerActivityHook.INSTANCE.initOnce(lpparam.classLoader);
     }
 }
---
+++ app/src/main/java/ltd/nextalone/pkginstallerplus/sdk30/PackageInstallerActivityHook30.kt
@@ -0,0 +1,85 @@
+package ltd.nextalone.pkginstallerplus.sdk30
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.graphics.Typeface
+import android.os.Build
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers.getObjectField
+import ltd.nextalone.pkginstallerplus.*
+
+object PackageInstallerActivityHook30 {
+
+    @SuppressLint("PrivateApi")
+    fun initOnce(cl: ClassLoader) {
+        if (cl.hasClass("com.android.packageinstaller.PackageInstallerActivity")) {
+            cl.loadClass("com.android.packageinstaller.PackageInstallerActivity")
+                .getDeclaredMethod("startInstallConfirm").hook(object : XC_MethodHook() {
+                    @RequiresApi(Build.VERSION_CODES.P)
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        val ctx: Activity = param.thisObject as Activity
+                        val confirmId = ctx.resources.getIdentifier("install_confirm_question_update", "id", "com.android.packageinstaller")
+                        val confirm: View? = ctx.findViewById(confirmId)
+                        if (confirm != null) {
+                            replaceSpacerWithInfoView(confirm, ctx)
+                        } else {
+                            Log.e(TAG, "spacer view not found")
+                        }
+                    }
+                })
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun replaceSpacerWithInfoView(confirm: View, activity: Activity) {
+        val context = confirm.context
+        val textView = TextView(context)
+        textView.setTextIsSelectable(true)
+        textView.typeface = Typeface.MONOSPACE
+        val layout = LinearLayout(context)
+        val newPkgInfo: PackageInfo = getObjectField(activity, "mPkgInfo") as PackageInfo
+        val pkgName = newPkgInfo.packageName
+        val oldPkgInfo = try {
+            activity.packageManager.getPackageInfo(pkgName, PackageManager.MATCH_UNINSTALLED_PACKAGES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            null
+        }
+        val sb = SpannableStringBuilder()
+        val greenSpan = ForegroundColorSpan(ThemeUtil.colorGreen)
+        val redSpan = ForegroundColorSpan(ThemeUtil.colorRed)
+        if (oldPkgInfo == null) {
+            val oldVersionStr =
+                (newPkgInfo.versionName ?: "N/A") + "(" + newPkgInfo.longVersionCode + ")"
+            sb.append("PackageName:\n")
+                .append(" +$pkgName", greenSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                .append('\n')
+                .append(" +$oldVersionStr", greenSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            layout.setPadding(0, context.dip2px(21f), 0, 0)
+        } else {
+            val oldVersionStr = """${oldPkgInfo.versionName ?: "N/A"}(${oldPkgInfo.longVersionCode})"""
+            val newVersionStr = """${newPkgInfo.versionName ?: "N/A"}(${newPkgInfo.longVersionCode})"""
+            sb.append("PackageName:\n")
+                .append("  $pkgName\n")
+                .append("Version:\n")
+                .append(" +$newVersionStr", greenSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                .append('\n')
+                .append(" -$oldVersionStr", redSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            layout.setPadding(0, context.dip2px(45f), 0, 0)
+        }
+        textView.text = sb
+
+        layout.addView(textView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        (confirm.parent as ViewGroup).addView(layout)
+    }
+}
